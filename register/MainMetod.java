644c5cda0428047d251a85462cfc3d9886c88613
peoject Demo <virajakte2001@gmail.com>
2025-09-14 15:38:23 +0530

    added

---
+++ register/MainMetod.java
@@ -0,0 +1,11 @@
+package com.register;
+
+public class MainMetod {
+	
+	public static void main(String[] args) {
+    InputStudentdata isd=new InputStudentdata();
+    isd.inputStudentData();
+	}
+	
+
+}
