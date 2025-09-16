c4d584093ff1f018b3535dba40962d46f321bbe5
sam3405 <sammaner33@gmail.com>
2025-09-16 12:10:42 +0530

    User Stroy 3.1

---
+++ QuizMaster/src/com/velocity/quizmaster/addquestions/AddQuestion.java
@@ -0,0 +1,51 @@
+package com.velocity.quizmaster.addquestions;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class AddQuestion  {
+
+	private static final String DB_Driver = "com.mysql.cj.jdbc.Driver";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/quizmaster";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "root";
+
+	public Connection getConnection() {
+		Connection con = null;
+		try {
+			// load the Driver
+			Class.forName(DB_Driver);
+			// establish the connection
+			con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		return con;
+
+	}
+
+	public boolean addQuestion(Questions question) {
+		String sql = "INSERT INTO question (question_text, option1, option2, option3, option4, correct_option) VALUES (?, ?, ?, ?, ?, ?)";
+		PreparedStatement pstmt = null;
+		try{
+
+			Connection conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, question.getQuestionText());
+			pstmt.setString(2, question.getOption1());
+			pstmt.setString(3, question.getOption2());
+			pstmt.setString(4, question.getOption3());
+			pstmt.setString(5, question.getOption4());
+			pstmt.setString(6, question.getCorrectOption());
+
+			int rows = pstmt.executeUpdate();
+			return rows > 0; // true if insertion worked
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+		
+	}
+}
---
+++ QuizMaster/src/com/velocity/quizmaster/addquestions/Questions.java
@@ -0,0 +1,49 @@
+package com.velocity.quizmaster.addquestions;
+
+public class Questions {
+
+	
+	private String questionText;
+	private String option1;
+	private String option2;
+	private String option3;
+	private String option4;
+	private String correctOption;
+
+	// Constructor
+	public Questions(String questionText, String option1, String option2, String option3, String option4,
+			String correctOption) {
+		this.questionText = questionText;
+		this.option1 = option1;
+		this.option2 = option2;
+		this.option3 = option3;
+		this.option4 = option4;
+		this.correctOption = correctOption;
+	}
+
+	// Getters 
+	public String getQuestionText() {
+		return questionText;
+	}
+
+	public String getOption1() {
+		return option1;
+	}
+
+	public String getOption2() {
+		return option2;
+	}
+
+	public String getOption3() {
+		return option3;
+	}
+
+	public String getOption4() {
+		return option4;
+	}
+
+	public String getCorrectOption() {
+		return correctOption;
+	}
+
+}
