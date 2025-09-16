72400b14a41c38714df5c99613a9c7c67d2480bc
sam3405 <sammaner33@gmail.com>
2025-09-16 20:54:20 +0530

    user story 3.3

---
+++ QuizMaster/src/com/velocity/quizmaster/scorebyid/StudentScoreById.java
@@ -0,0 +1,63 @@
+package com.velocity.quizmaster.scorebyid;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class StudentScoreById {//Search Student Score by ID (User Story 3.3)
+
+	private static final String DB_Driver = "com.mysql.cj.jdbc.Driver";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/quizmaster";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "root";
+
+	public Connection getConnection() {
+		Connection con = null;
+		try {
+			// load the Driver
+			Class.forName(DB_Driver);
+			// establish the connection
+			con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		return con;
+
+	}
+
+	public void getScore(int studentId) {
+		String query = "SELECT total_score, grade FROM score WHERE student_id = ?";
+
+		try {
+
+			Connection conn = getConnection();
+			PreparedStatement ps = conn.prepareStatement(query);
+
+			ps.setInt(1, studentId);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				int score = rs.getInt("total_score");
+				String grade = rs.getString("grade");
+
+				System.out.println("Score: " + score);
+				System.out.println("Grade: " + grade);
+			} else {
+				System.out.println("No record found for Student ID: " + studentId);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Fetch Score by Student ID");
+//		System.out.print("Enter Student ID: ");
+//		int studentId = sc.nextInt();
+//		getScore(studentId);
+//	}
+
+}
