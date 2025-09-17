1baf166f161e72700dba84f1d1e5f9da4f2922d0
peoject Demo <virajakte2001@gmail.com>
2025-09-17 09:36:51 +0530

    score analysis

---
+++ Project/src/com/velocity/quizmaster/score/ScoreAnalysis.java
@@ -0,0 +1,32 @@
+package com.velocity.quizmaster.score;
+
+public class ScoreAnalysis {
+	
+	public static String calculateGradeAndMessage(int score) {
+		
+		String grade;
+		String message;
+		
+		if (score >= 8) {
+            grade = "A";
+            message = "Excellent";
+        } else if (score >= 5) {
+            grade = "B";
+            message = "Good";
+        } else {
+            grade = "C";
+            message = "Needs Improvement";
+        }
+		
+		// Display to student
+        System.out.println("=== Quiz Result ===");
+        System.out.println("Your Score: " + score + " / 10");
+        System.out.println("Your Grade: " + grade);
+        System.out.println("Feedback: " + message);
+
+        return grade; // return grade for DB storage
+		
+		
+	}
+
+}
